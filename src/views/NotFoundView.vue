a73460c31192e9158945eac427ead3157312e74c
ahilarion <alan.hilarion@proton.me>
2024-10-28 07:37:30 +0100

    fix(i18n) : fix NotFoundView.vue trad

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -6,7 +6,7 @@ import RouterButton from "@/components/commons/RouterButton.vue";
   <div class="flex flex-col items-center justify-center gap-4 text-center">
     <img src="@/assets/404.png" alt="404" class="h-96 w-96" />
     <h1>{{ $t('errors.404') }}</h1>
-    <RouterButton :label="$t('actions.back')" to="/" />
+    <RouterButton :label="$t('common.actions.back')" to="/" />
   </div>
 </template>
 
